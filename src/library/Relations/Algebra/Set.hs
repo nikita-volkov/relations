fa27cc28442eb88021069422f044c97405195e82
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-10-08 23:28:55 +0300

    Simplify

--- src/library/Relations/Algebra/Set.hs
+++ src/library/Relations/Algebra/Set.hs
@@ -1,32 +1,12 @@
 module Relations.Algebra.Set
   ( IsSetOf (..),
-    underUnion,
-    SetUnderUnion,
-    fromUnderUnion,
   )
 where
 
-import Data.Semigroup
+import Relations.Algebra.List
 import Prelude
 
-class IsSetOf element set | set -> element where
+class (IsListOf element set) => IsSetOf element set | set -> element where
   contains :: element -> set -> Bool
   include :: element -> set -> set
   exclude :: element -> set -> set
-  nil :: set
-  unite :: set -> set -> set
-  intersect :: set -> set -> set
-
-fromUnderUnion :: SetUnderUnion set element -> set
-fromUnderUnion (SetUnderUnion s) = s
-
-newtype SetUnderUnion set element = SetUnderUnion set
-
-instance (IsSetOf element set) => Semigroup (SetUnderUnion set element) where
-  SetUnderUnion x <> SetUnderUnion y = SetUnderUnion (unite x y)
-
-instance (IsSetOf element set) => Monoid (SetUnderUnion set element) where
-  mempty = SetUnderUnion nil
-
-underUnion :: (IsSetOf element set) => set -> SetUnderUnion set element
-underUnion = SetUnderUnion
