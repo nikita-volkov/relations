3322262ba3c9a8b460ff0aba2603a253207f2b26
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-10-09 08:36:26 +0300

    Add instances module

--- src/library/Relations.hs
+++ src/library/Relations.hs
@@ -4,10 +4,4 @@ module Relations
 where
 
 import Relations.Algebra
-import Relations.Instances.ByteString ()
-import Relations.Instances.HashSet ()
-import Relations.Instances.LazyByteString ()
-import Relations.Instances.LazyText ()
-import Relations.Instances.Set ()
-import Relations.Instances.String ()
-import Relations.Instances.Text ()
+import Relations.Instances ()
---
+++ src/library/Relations/Instances.hs
@@ -0,0 +1,9 @@
+module Relations.Instances () where
+
+import Relations.Instances.ByteString ()
+import Relations.Instances.HashSet ()
+import Relations.Instances.LazyByteString ()
+import Relations.Instances.LazyText ()
+import Relations.Instances.Set ()
+import Relations.Instances.String ()
+import Relations.Instances.Text ()
